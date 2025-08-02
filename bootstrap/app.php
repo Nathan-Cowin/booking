5db5e2c9ffe3245d00af2a7c1b6043fa985d7e7d
nathan <nathancowin01@gmail.com>
2025-08-02 12:50:46 +0100

    set up tenant aware routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Multitenancy\Http\Middleware\EnsureValidTenantSession;
+use Spatie\Multitenancy\Http\Middleware\NeedsTenant;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware
+            ->web(append: [
+                NeedsTenant::class,
+                EnsureValidTenantSession::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
